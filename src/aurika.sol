97a4fe49738fb5f4904d18bffd309dac3e626984
yashgoel <yash.goel8370@gmail.com>
2025-06-28 22:52:24 +0530

    Dashboard Changes according to updated contract

--- src/aurika.sol
+++ src/aurika.sol
@@ -3,82 +3,77 @@ pragma solidity ^0.8.16;
 
 contract Aurika {
     struct Order {
-        string date;
-        string time;
-        bool isBuyOrder; // true = buy, false = sell
-        uint256 gram;
+        bool isBuyOrder;
+        uint256 quantity;
         uint256 avgPrice;
-        string txhash;
-        string ipfsPdfReceipt;
     }
 
     struct User {
         uint256 invested;
-        uint256 grams;
+        uint256 quantity;
         Order[] orders;
     }
 
     mapping(address => User) public users;
 
-    event OrderPlaced(
-        address indexed user,
-        bool isBuyOrder,
-        uint256 gram,
-        uint256 avgPrice,
-        uint256 totalPrice,
-        string txhash,
-        string ipfsPdfReceipt
-    );
+    event OrderPlaced(address indexed user, bool isBuyOrder, uint256 quantity, uint256 avgPrice, uint256 totalPrice);
+    event Gifted(address indexed from, address indexed to, uint256 quantity, uint256 avgPrice, uint256 totalPrice);
 
-    function addOrder(
-        string memory _date,
-        string memory _time,
-        bool _isBuyOrder,
-        uint256 _gram,
-        uint256 _avgPrice,
-        string memory _txhash,
-        string memory _ipfsPdfReceipt
-    ) external payable {
-        uint256 totalPrice = _gram * _avgPrice;
+    function addOrder(bool _isBuyOrder, uint256 _quantity, uint256 _avgPrice) external payable {
+        require(_quantity > 0 && _avgPrice > 0, "Invalid inputs");
+        uint256 totalPrice = _quantity * _avgPrice;
 
         Order memory newOrder = Order({
-            date: _date,
-            time: _time,
             isBuyOrder: _isBuyOrder,
-            gram: _gram,
-            avgPrice: _avgPrice,
-            txhash: _txhash,
-            ipfsPdfReceipt: _ipfsPdfReceipt
+            quantity: _quantity,
+            avgPrice: _avgPrice
         });
+
+        User storage user = users[msg.sender];
 
         if (_isBuyOrder) {
             require(msg.value == totalPrice, "Incorrect ETH sent for purchase");
-            users[msg.sender].invested += totalPrice;
-            users[msg.sender].grams += _gram;
+            user.invested += totalPrice;
+            user.quantity += _quantity;
         } else {
-            require(users[msg.sender].grams >= _gram, "Not enough grams to sell");
-            users[msg.sender].invested -= totalPrice;
-            users[msg.sender].grams -= _gram;
-
-            require(address(this).balance >= totalPrice, "Contract doesn't have enough ETH");
+            require(user.quantity >= _quantity, "Not enough quantity to sell");
+            user.invested -= totalPrice;
+            user.quantity -= _quantity;
             payable(msg.sender).transfer(totalPrice);
         }
 
-        users[msg.sender].orders.push(newOrder);
+        user.orders.push(newOrder);
 
-        emit OrderPlaced(
-            msg.sender,
-            _isBuyOrder,
-            _gram,
-            _avgPrice,
-            totalPrice,
-            _txhash,
-            _ipfsPdfReceipt
-        );
+        emit OrderPlaced(msg.sender, _isBuyOrder, _quantity, _avgPrice, totalPrice);
     }
 
-    function getOrders() public view returns (Order[] memory) {
-        return users[msg.sender].orders;
+    function gift(uint256 _quantity, uint256 _avgPrice, address walletAddress) external payable {
+        require(_quantity > 0 && _avgPrice > 0, "Invalid input values");
+
+        uint256 totalPrice = _quantity * _avgPrice;
+        require(msg.value == totalPrice, "Incorrect ETH sent for gift");
+
+        users[walletAddress].invested += totalPrice;
+        users[walletAddress].quantity += _quantity;
+
+        Order memory newOrder = Order({
+            isBuyOrder: true,
+            quantity: _quantity,
+            avgPrice: _avgPrice
+        });
+
+        users[walletAddress].orders.push(newOrder);
+
+        emit Gifted(msg.sender, walletAddress, _quantity, _avgPrice, totalPrice);
+    }
+
+    function getOrderAt(uint256 index) public view returns (bool, uint256, uint256) {
+        Order memory order = users[msg.sender].orders[index];
+        return (order.isBuyOrder, order.quantity, order.avgPrice);
+    }
+
+    function getOrderCount() public view returns (uint256) {
+        return users[msg.sender].orders.length;
     }
 
     receive() external payable {}
