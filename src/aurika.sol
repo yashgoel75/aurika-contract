1bd1e3562810ffc9d44f2aa22883a064a21fe44c
yashgoel <yash.goel8370@gmail.com>
2025-06-30 07:13:51 +0530

    commit

--- src/aurika.sol
+++ src/aurika.sol
@@ -1,81 +1,87 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+import "./getPrice.sol";
+
 contract Aurika {
     struct Order {
-        bool isBuyOrder;
-        uint256 quantity;
-        uint256 avgPrice;
+        string orderType; // Buy/Sell/Gift
+        uint256 quantity; // in micrograms (μg)
+        uint256 avgPrice; // μg per ETH * 1000
     }
 
     struct User {
-        uint256 invested;
-        uint256 quantity;
+        uint256 invested; // in wei
+        uint256 quantity; // in micrograms (μg)
         Order[] orders;
     }
 
     mapping(address => User) public users;
 
-    event OrderPlaced(address indexed user, bool isBuyOrder, uint256 quantity, uint256 avgPrice, uint256 totalPrice);
-    event Gifted(address indexed from, address indexed to, uint256 quantity, uint256 avgPrice, uint256 totalPrice);
+    event Buy(address indexed user, uint256 quantity, uint256 value);
+    event Sell(address indexed user, uint256 quantity, uint256 value);
+    event Gift(address indexed from, address indexed to, uint256 quantity, uint256 value);
 
-    function addOrder(bool _isBuyOrder, uint256 _quantity, uint256 _avgPrice) external payable {
-        require(_quantity > 0 && _avgPrice > 0, "Invalid inputs");
-        uint256 totalPrice = _quantity * _avgPrice;
+    function getAveragePrice() public view returns (uint256) {
+        uint256 ethUsd = getPrice.getEthUsd();  // 18 decimals
+        uint256 xauUsd = getPrice.getXauUsd();  // 18 decimals
+        return (ethUsd * 31103500) / xauUsd;    // result in μg per ETH * 1000
+    }
 
-        Order memory newOrder = Order({
-            isBuyOrder: _isBuyOrder,
-            quantity: _quantity,
-            avgPrice: _avgPrice
-        });
+    function buyOrder() external payable {
+        require(msg.value > 0, "No ETH sent");
+
+        uint256 avgPrice = getAveragePrice(); // μg per ETH * 1000
+        uint256 quantity = (msg.value * avgPrice) / 1e18; // μg
 
         User storage user = users[msg.sender];
+        user.invested += msg.value;
+        user.quantity += quantity;
 
-        if (_isBuyOrder) {
-            require(msg.value == totalPrice, "Incorrect ETH sent for purchase");
-            user.invested += totalPrice;
-            user.quantity += _quantity;
+        user.orders.push(Order("Buy", quantity, avgPrice));
+        emit Buy(msg.sender, quantity, msg.value);
+    }
+
+    function sellOrder(uint256 quantity) external {
+        User storage user = users[msg.sender];
+        require(user.quantity >= quantity, "Insufficient gold");
+
+        uint256 avgPrice = getAveragePrice(); // μg per ETH * 1000
+        uint256 refund = (quantity * 1e18) / avgPrice;
+
+        user.quantity -= quantity;
+        if (user.invested >= refund) {
+            user.invested -= refund;
         } else {
-            require(user.quantity >= _quantity, "Not enough quantity to sell");
-            user.invested -= totalPrice;
-            user.quantity -= _quantity;
-            payable(msg.sender).transfer(totalPrice);
+            user.invested = 0;
         }
 
-        user.orders.push(newOrder);
+        user.orders.push(Order("Sell", quantity, avgPrice));
+        payable(msg.sender).transfer(refund);
 
-        emit OrderPlaced(msg.sender, _isBuyOrder, _quantity, _avgPrice, totalPrice);
+        emit Sell(msg.sender, quantity, refund);
     }
 
-    function gift(uint256 _quantity, uint256 _avgPrice, address walletAddress) external payable {
-        require(_quantity > 0 && _avgPrice > 0, "Invalid input values");
+    function gift(address recipient) external payable {
+        require(msg.value > 0, "No ETH sent");
 
-        uint256 totalPrice = _quantity * _avgPrice;
-        require(msg.value == totalPrice, "Incorrect ETH sent for gift");
+        uint256 avgPrice = getAveragePrice();
+        uint256 quantity = (msg.value * avgPrice) / 1e18;
 
-        users[walletAddress].invested += totalPrice;
-        users[walletAddress].quantity += _quantity;
+        User storage user = users[recipient];
+        user.invested += msg.value;
+        user.quantity += quantity;
+        user.orders.push(Order("Gift", quantity, avgPrice));
 
-        Order memory newOrder = Order({
-            isBuyOrder: true,
-            quantity: _quantity,
-            avgPrice: _avgPrice
-        });
-
-        users[walletAddress].orders.push(newOrder);
-
-        emit Gifted(msg.sender, walletAddress, _quantity, _avgPrice, totalPrice);
+        emit Gift(msg.sender, recipient, quantity, msg.value);
     }
 
-    function getOrderAt(uint256 index) public view returns (bool, uint256, uint256) {
+    function getOrderAt(uint256 index) public view returns (string memory, uint256, uint256) {
         Order memory order = users[msg.sender].orders[index];
-        return (order.isBuyOrder, order.quantity, order.avgPrice);
+        return (order.orderType, order.quantity, order.avgPrice);
     }
 
     function getOrderCount() public view returns (uint256) {
         return users[msg.sender].orders.length;
     }
-
-    receive() external payable {}
-    fallback() external payable {}
 }
--- src/getPrice.sol
+++ src/getPrice.sol
@@ -3,22 +3,22 @@ pragma solidity ^0.8.16;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-contract getPrice {
+library getPrice {
 
     address constant ethUsdFeedAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
     address constant xauUsdFeedAddress = 0xC5981F461d74c46eB4b0CF3f4Ec79f025573B0Ea;
+
     function getEthUsd() internal view returns (uint256) {
         AggregatorV3Interface feed = AggregatorV3Interface(ethUsdFeedAddress);
         (, int256 ethUsd,,,) = feed.latestRoundData();
         require(ethUsd > 0, "Invalid price feed");
-        return uint256(ethUsd);
+        return uint256(ethUsd) * 1e10; // scale from 8 to 18 decimals
     }
 
     function getXauUsd() internal view returns (uint256) {
         AggregatorV3Interface feed = AggregatorV3Interface(xauUsdFeedAddress);
         (, int256 xauUsd,,,) = feed.latestRoundData();
         require(xauUsd > 0, "Invalid price feed");
-        return uint256(xauUsd);
+        return uint256(xauUsd) * 1e10; // scale from 8 to 18 decimals
     }
-    
 }
