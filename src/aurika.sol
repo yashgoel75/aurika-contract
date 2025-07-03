5273d2bd73fc37e58c518d684f3a996de0db1274
yashgoel <yash.goel8370@gmail.com>
2025-07-03 14:04:00 +0530

    commit

--- src/aurika.sol
+++ src/aurika.sol
@@ -20,12 +20,17 @@ contract Aurika {
 
     event Buy(address indexed user, uint256 quantity, uint256 value);
     event Sell(address indexed user, uint256 quantity, uint256 value);
-    event Gift(address indexed from, address indexed to, uint256 quantity, uint256 value);
+    event Gift(
+        address indexed from,
+        address indexed to,
+        uint256 quantity,
+        uint256 value
+    );
 
     function getAveragePrice() public view returns (uint256) {
-        uint256 ethUsd = getPrice.getEthUsd();  // 18 decimals
-        uint256 xauUsd = getPrice.getXauUsd();  // 18 decimals
-        return (ethUsd * 31103500) / xauUsd;    // result in μg per ETH * 1000
+        uint256 ethUsd = getPrice.getEthUsd(); // 18 decimals
+        uint256 xauUsd = getPrice.getXauUsd(); // 18 decimals
+        return (ethUsd * 31103500) / xauUsd; // result in μg per ETH * 1000
     }
 
     function buyOrder() external payable {
@@ -76,7 +81,9 @@ contract Aurika {
         emit Gift(msg.sender, recipient, quantity, msg.value);
     }
 
-    function getOrderAt(uint256 index) public view returns (string memory, uint256, uint256) {
+    function getOrderAt(
+        uint256 index
+    ) public view returns (string memory, uint256, uint256) {
         Order memory order = users[msg.sender].orders[index];
         return (order.orderType, order.quantity, order.avgPrice);
     }
